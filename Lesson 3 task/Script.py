b4ed9e58642af15f8d8c9b88ae1beb62ea6589b6
Екатерина Кокшарова <e.koksharova@ticketland.ru>
2020-01-23 18:18:11 +0300

    dz3

---
+++ Lesson 3 task/Script.py
@@ -0,0 +1,24 @@
+import csv
+import json
+from context_manager_methods import ClassContextManager
+
+cm_obj = ClassContextManager()
+
+result = []
+books = cm_obj.books_csv_reader()
+
+for each_user in cm_obj.users_json_reader():
+
+    book_info = next(books)
+    result.append({
+        "name": each_user['name'],
+        "gender": each_user['gender'],
+        "address": each_user['address'],
+        "books": {
+            "title": book_info[0],
+            "author": book_info[1],
+            "height": book_info[3]
+        }
+    })
+
+cm_obj.output_json_writer(result)
---
+++ Lesson 3 task/context_manager_methods.py
@@ -0,0 +1,32 @@
+import csv
+import json
+
+class ClassContextManager:
+
+    pathcsv = 'Data/books.csv'
+    pathjson = 'Data/users.json'
+    pathjson_output = 'Lesson 3 task/output.json'
+
+    def books_csv_reader(self):
+        """ Read data from a CSV file """
+
+        with open(self.pathcsv, "r") as books:
+            csvreader = csv.reader(books)
+            next(csvreader)
+            for row in csvreader:
+                yield row
+
+    def users_json_reader(self):
+        """ Read data from a JSON file """
+
+        with open(self.pathjson, "r") as users:
+            jsonreader = json.load(users)
+            for each_user in jsonreader:
+                yield each_user
+
+    def output_json_writer(self, result):
+        """ Write data to a JSON file """
+
+        with open(self.pathjson_output, "w") as books_and_users:
+            json.dump(result, books_and_users, indent=4)
+            
